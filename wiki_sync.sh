6213549b9fca3faa13cee58537d510581423719a
kijima <norimaking777@gmail.com>
2020-02-01 22:55:09 +0900

    .crontabを追加した

--- wiki_sync.sh
+++ wiki_sync.sh
@@ -5,5 +5,5 @@
 pc="/home/kijima/Dropbox/pages/"
 server="xsrv:/home/kijima9791/a9ne.com/public_html/wiki/data/pages/"
 
-rsync -auvz --delete $pc $server
-rsync -auvz --delete $server $pc
+rsync -auvz $pc $server
+rsync -auvz --delete $server $pc # 削除はサーバ側からのみ可能。
