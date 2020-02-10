87455e1ff48257439d639fd2f06f46b1b00b88c7
kijima <norimaking777@gmail.com>
2020-02-11 02:24:52 +0900

    .gitを除外するようにした。

--- wiki_sync.sh
+++ wiki_sync.sh
@@ -4,6 +4,7 @@
 
 pc="/home/kijima/Dropbox/pages/"
 server="xsrv:/home/kijima9791/a9ne.com/public_html/wiki/data/pages/"
+exc=".git"
 
-rsync -auvz --delete $server $pc # 削除はサーバ側からのみ可能。
-rsync -auvz $pc $server
+rsync -auvz --exclude $exc $pc $server
+rsync -auvz --delete --exclude $exc $server $pc # 削除はサーバ側からのみ可能。
