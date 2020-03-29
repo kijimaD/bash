b3fe0df044923d6d9868172dbb99de16d95627ff
kijima <norimaking777@gmail.com>
2020-03-29 11:31:23 +0900

    引数化した

--- doku_wc.sh
+++ doku_wc.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
 # Record characters in directory
-# 保存ディレクトリは外部ファイル化する!
+# 保存ディレクトリは引数化する。
 
-count_dir="/home/kijima/Dropbox/pages/"
-output_file="/home/kijima/Project/htdocs/wc.csv"
+count_dir=$1
+# count_dir="/home/kijima/Dropbox/pages/"
+
+output_file=$2
+# output_file="/home/kijima/Project/htdocs/wc.csv"
 
 c_count=`find $count_dir -type f -not -name 'wc.csv' | xargs cat | wc -m`
 today_date=`date +"%Y,%m,%d"`
